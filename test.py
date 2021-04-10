4352f776f2962bd9fde4bbaa31b6b564d9be7ba2
unknown <dddgggbbb@gmail.com>
2021-04-10 14:23:48 -0400

    2nd commit

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+from sys import argv, exit, stderr
+from app import app
+import logging
+
+def main(argv):
+    print("Hello World")
+    exit(0)
+
+if __name__ == '__main__':
+  main(argv)
